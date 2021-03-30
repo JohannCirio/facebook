bffb0884a1179ad94d5aaebd07f8984b7c22d9df
Johann <johannocsantos@gmail.com>
2021-03-30 18:13:00 -0300

    Removes index route from User an adds routes for friendships

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root to: "posts#index"
   resources :comments, only: [:create, :edit, :update, :destroy]
   resources :posts
-  resources :users, only: [:index]
-  resources :friendships, only: [:create, :destroy, :update]
+  resources :friendships, only: [:index, :create, :destroy, :update]
 
   devise_for :users
 end
