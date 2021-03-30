8df0e934088de2a67dc3ecbdae7a85e972b66ee9
Johann <johannocsantos@gmail.com>
2021-03-30 09:38:17 -0300

    Adds friendship and inverse friendship user model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,11 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :comments
+
+  has_many :friendships
+  has_many :friends, through: :friendships
+
+  # how to see who befriended the user
+  has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+  has_many :inverse_friends, through: :inverse_friendships, source: :user
 end
