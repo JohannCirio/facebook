05daf38b219704b480d1e18cb9d63b053ed2ffb5
Johann <johannocsantos@gmail.com>
2021-03-31 17:25:26 -0300

    Adds routes for likes model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   root to: "posts#index"
-  resources :comments, only: [:create, :edit, :update, :destroy]
-  resources :posts
+  resources :comments, only: [:create, :edit, :update, :destroy] do
+    resources :likes, only: [:create, :destroy]
+  end
+  resources :posts do
+    resources :likes, only: [:create, :destroy]
+  end
   resources :friendships, only: [:index, :create, :destroy, :update]
-
+  
   devise_for :users
 end
