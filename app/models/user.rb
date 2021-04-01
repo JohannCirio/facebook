259e54790ec8f0e0e3fa0bb7d339fda76d2e390a
Johann <johannocsantos@gmail.com>
2021-04-01 14:45:45 -0300

    Adds photo attachment via Active Storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,7 @@ class User < ApplicationRecord
   # how to see who befriended the user
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
   has_many :inverse_friends, through: :inverse_friendships, source: :user
+
+  # image attachment
+  has_one_attached :avatar
 end
